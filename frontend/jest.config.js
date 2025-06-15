557acec9265e80e28f59856049f55b9c82c45e61
kai-5908 <dolce.cantabile.maestoso@gmail.com>
2025-06-15 17:45:51 +0900

    jest.config.jsを削除し、jest.config.cjsに統一

--- frontend/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
-};
