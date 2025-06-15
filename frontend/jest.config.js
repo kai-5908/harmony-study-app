77cf642e4183bbc74165b12b9aa56703ab3c9182
kai-5908 <dolce.cantabile.maestoso@gmail.com>
2025-06-15 17:38:38 +0900

    chore(frontend): add Vite+React+TS initial files, config, and frontend CI workflow

---
+++ frontend/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+};
